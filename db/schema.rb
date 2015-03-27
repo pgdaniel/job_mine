d8dcca366c7ccb54c6999daf8fd65716c952a873
Paul Daniel <paulgdan@gmail.com>
2015-03-27 13:22:15 -0700

    added Indeed Job Search

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150322225600) do
+ActiveRecord::Schema.define(version: 20150323145315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20150322225600) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.string   "type"
+    t.string   "city"
   end
 
   add_index "listings", ["search_term_id"], name: "index_listings_on_search_term_id", using: :btree
